e8b363ad86cb19d1126ba39e49b5f09ff75f4d2d
quaid-perkins <250244@colegsirgar.ac.uk>
2025-10-02 14:20:02 +0100

    feat: useSizeBreakpoints

---
+++ src/hooks/useSizeBreakpoints.ts
@@ -0,0 +1,19 @@
+import { createBreakpoints, Matches } from "@solid-primitives/media";
+
+/*
+	screen size breakpoints
+	marked as true when the screen size is equal to or greater than the given pixel size
+	values match tailwindcss - https://tailwindcss.com/docs/responsive-design
+*/
+const breakpoints = {
+	sm: "640px",
+	md: "768px",
+	lg: "1024px",
+	xl: "1280px",
+};
+
+// screen size breakpoints for device dependent components
+const useSizeBreakpoints = (): Matches<typeof breakpoints> =>
+	createBreakpoints(breakpoints);
+
+export default useSizeBreakpoints;
