9e467226af5f474f6adf9bfa9c83227962b7c27d
quaid-perkins <250244@colegsirgar.ac.uk>
2025-10-02 22:27:22 +0100

    chore: footer text center

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import { Component } from "solid-js";
 
 const Footer: Component = () => {
 	return (
-		<footer class="inline-flex justify-center w-full p-4 mt-auto text-sm">
+		<footer class="inline-flex justify-center text-center w-full p-4 mt-auto text-sm">
 			Copyright © 2025 Quaid Perkins. All Rights Reserved
 		</footer>
 	);
