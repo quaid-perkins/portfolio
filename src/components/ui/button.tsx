a65b82ef9aa7d521edf6ce9a6a22aa224040b70e
quaid-perkins <250244@colegsirgar.ac.uk>
2025-10-02 18:51:47 +0100

    chore: buttons have normal font thickness

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,7 +9,7 @@ import { splitProps } from "solid-js";
 
 // button component from shadcn-solid - https://shadcn-solid.com/docs/components/button
 export const buttonVariants = cva(
-	"inline-flex items-center justify-center rounded-md font-medium transition-[color,background-color,box-shadow] cursor-pointer focus-visible:outline-none focus-visible:ring-[1.5px] focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+	"inline-flex items-center justify-center rounded-md transition-[color,background-color,box-shadow] cursor-pointer focus-visible:outline-none focus-visible:ring-[1.5px] focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
 	{
 		variants: {
 			variant: {
