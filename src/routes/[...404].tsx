65b6be2c6c302786acf2a12e2475360109e2c237
quaid-perkins <250244@colegsirgar.ac.uk>
2025-10-08 15:56:31 +0100

    feat: basic page layout

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,5 +1,8 @@
 import { Title } from "@solidjs/meta";
+import { A } from "@solidjs/router";
 import { HttpStatusCode } from "@solidjs/start";
+import { ArrowLeftToLine, TriangleAlert } from "lucide-solid";
+import { Button } from "~/components/ui/button";
 
 // not found 404 error page
 const NotFound = () => {
@@ -7,7 +10,35 @@ const NotFound = () => {
     <>
       <Title>Not Found</Title>
       <HttpStatusCode code={404} />
-      <main></main>
+      <main class="mx-auto my-8 flex w-full flex-col gap-8 max-sm:px-1 sm:w-[50%] sm:justify-between">
+        <div class="flex flex-col">
+          <div class="my-2 text-7xl font-bold sm:text-8xl">
+            {"Error: "}
+            <span class="text-purple-900">404</span>
+          </div>
+          <span class="pl-1 text-xl sm:text-2xl">
+            This page doesn't seem to exist.
+            <span class="text-[#6DA34D]">{" D:"}</span>
+          </span>
+        </div>
+        <div class="flex justify-end gap-2 px-1">
+          <Button
+            as="a"
+            href="https://github.com/quaid-perkins/portfolio/issues"
+            target="_blank"
+            title="report"
+            variant="ghost"
+            class="hover:bg-red-900 hover:text-zinc-50"
+          >
+            <TriangleAlert size={20} />
+            Report
+          </Button>
+          <Button as={A} href="/" title="home">
+            <ArrowLeftToLine size={20} />
+            Return Home
+          </Button>
+        </div>
+      </main>
     </>
   );
 };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,29 +7,38 @@ const Home = () => {
   return (
     <>
       <Title>Home</Title>
-      <main class="mx-auto my-8 sm:w-[80vw] w-full flex flex-col gap-16 sm:justify-between max-sm:px-2">
+      <main class="mx-auto my-8 flex w-full flex-col gap-8 max-sm:px-1 sm:w-[50%] sm:justify-between">
         <div class="flex flex-col">
-					<span class="inline-flex items-center gap-1 pl-1">
-						<MapPin size={20} />
-						{" Wales, UK"}
-					</span>
-					<div class="font-bold text-8xl my-2">
-						Quaid
-						<span class="text-purple-900">{" Perkins"}</span>
-					</div>
-					<span class="pl-1 text-xl">
-						Computer Engineering and I.T. Student
-					</span>
-				</div>
-				<div class="flex flex-col gap-4 items-end">
-          <span class="w-[90%] sm:w-[65%] text-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur, in natus explicabo nobis cumque voluptatem ullam esse nihil sequi reprehenderit aliquid laborum fuga molestias inventore totam. Voluptatum aperiam provident odit!</span>
-          <div class="flex gap-2 justify-end">
+          <span class="inline-flex items-center gap-1 pl-1">
+            <MapPin size={20} />
+            <span>
+              <span class="text-[#6DA34D]">Wales</span>
+              {", UK"}
+            </span>
+          </span>
+          <div class="my-2 text-7xl font-bold sm:text-8xl">
+            Quaid
+            <span class="text-purple-900">{" Perkins"}</span>
+          </div>
+          <span class="pl-1 text-xl sm:text-2xl">
+            Computer Engineering & I.T. Student
+          </span>
+        </div>
+        <div class="flex flex-col items-end gap-4 px-1">
+          <span class="text-justify">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur,
+            in natus explicabo nobis cumque voluptatem ullam esse nihil sequi
+            reprehenderit aliquid laborum fuga molestias inventore totam.
+            Voluptatum aperiam provident odit!
+          </span>
+          <div class="flex justify-end gap-2">
             <Button
               as="a"
               href="https://github.com/quaid-perkins"
               target="_blank"
               variant="ghost"
               size="slim"
+              class="hover:bg-zinc-200"
             >
               <Github size={20} />
             </Button>
@@ -39,16 +48,16 @@ const Home = () => {
               target="_blank"
               variant="ghost"
               size="slim"
+              class="hover:bg-zinc-200"
             >
               <Linkedin size={20} />
             </Button>
-            <Button
-              title="Download CV"
-            >
-              <Download size={20} />Download CV
+            <Button title="Download CV">
+              <Download size={20} />
+              Download CV
             </Button>
           </div>
-				</div>
+        </div>
       </main>
     </>
   );
