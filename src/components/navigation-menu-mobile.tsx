264c3d8e4a1c32d2555c75a134baef8b96a63f51
quaid-perkins <250244@colegsirgar.ac.uk>
2025-10-08 19:17:14 +0100

    chore: tailwind class order

--- src/components/navigation-menu-mobile.tsx
+++ src/components/navigation-menu-mobile.tsx
@@ -9,7 +9,7 @@ export type Props = {
 };
 
 const NavigationMenuMobile: Component<Props> = (props) => {
-	const [isMenuOpen, toggleMenu] = createToggle(false);
+  const [isMenuOpen, toggleMenu] = createToggle(false);
 
   return (
     <>
@@ -17,9 +17,9 @@ const NavigationMenuMobile: Component<Props> = (props) => {
         <Menu size={20} />
       </Button>
       <div
-        class="flex flex-col absolute w-full h-[100dvh] overflow-y-auto top-0 right-0 bg-zinc-50 transition-transform duration-250 z-10"
+        class="absolute top-0 right-0 z-10 flex h-[100dvh] w-full flex-col overflow-y-auto bg-zinc-50 transition-transform duration-250"
         classList={{
-          "-translate-x-full": !isMenuOpen()
+          "-translate-x-full": !isMenuOpen(),
         }}
       >
         <div class="inline-flex w-full justify-between border-b border-zinc-200/80 bg-white/80 p-4">
@@ -30,11 +30,16 @@ const NavigationMenuMobile: Component<Props> = (props) => {
             <X size={20} />
           </Button>
         </div>
-        <div class="inline-flex flex-col items-end p-4 gap-4">
+        <div class="inline-flex flex-col items-end gap-4 p-4">
           {/* menu items for loop */}
           <For each={props.items}>
             {(item) => (
-              <Button as={A} href={item.href} variant="ghost" on:click={toggleMenu}>
+              <Button
+                as={A}
+                href={item.href}
+                variant="ghost"
+                on:click={toggleMenu}
+              >
                 {item.label}
               </Button>
             )}
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,23 +9,22 @@ import { cva } from "~/libs/cva";
 	button component variant configuration
 */
 export const buttonVariants = cva({
-  base: "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md cursor-pointer transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  base: "focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive inline-flex shrink-0 cursor-pointer items-center justify-center gap-2 rounded-md whitespace-nowrap transition-all outline-none focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0",
   variants: {
     variant: {
-      default:
-        "bg-purple-900 text-zinc-50 shadow-xs hover:bg-purple-900/90",
+      default: "bg-purple-900 text-zinc-50 shadow-xs hover:bg-purple-900/90",
       destructive:
-        "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
+        "bg-destructive hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60 text-white shadow-xs",
       outline:
-        "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
+        "bg-background hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 border shadow-xs",
       secondary:
-        "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
+        "bg-secondary text-secondary-foreground hover:bg-secondary/80 shadow-xs",
       ghost: "hover:bg-zinc-100",
       link: "underline-offset-4 hover:underline",
     },
     size: {
       default: "h-9 px-4 py-2 has-[>svg]:px-3",
-      sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
+      sm: "h-8 gap-1.5 rounded-md px-3 has-[>svg]:px-2.5",
       lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
       slim: "max-h-9 p-2",
     },
